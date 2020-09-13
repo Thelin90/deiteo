6f453e1db8b491a6411da9f4563cb65d54b5a9a0
Simon Thelin <simon.thelin90@gmail.com>
2020-09-13 16:23:01 +0100

    DEITEO-01777777777777777777777-Sync-Pipenv-Pipfile-To-Docker-Container (#2)
    Co-authored-by: Thelin90 <simon@gameanalytics.com>

--- tools/k8s/spark/create_local.sh
+++ tools/k8s/spark/create_local.sh
@@ -8,8 +8,3 @@ sleep 10
 kubectl create -f ./tools/k8s/spark/spark-worker-deployment.yaml
 
 minikube addons enable ingress
-kubectl apply -f ./tools/k8s/spark/minikube-ingress.yaml
-
-echo "Add an entry to /etc/hosts:"
-sleep 2
-echo "$(minikube ip) sparkkubernetes" | sudo tee -a /etc/hosts
